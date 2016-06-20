a2bec44311c59d3bce953c52d7979ed41d6603d3
jjhlzn <jinjunhang@hotmail.com>
2016-06-20 12:06:18 +0800

    重构

--- app/src/main/java/com/jinjunhang/onlineclass/ui/activity/BaseActivity.java
+++ app/src/main/java/com/jinjunhang/onlineclass/ui/activity/BaseActivity.java
@@ -27,7 +27,9 @@ import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.app.AppCompatActivity;
 
+import com.jinjunhang.framework.controller.SingleFragmentActivity;
 import com.jinjunhang.onlineclass.R;
 
 import com.jinjunhang.onlineclass.model.Album;
@@ -38,11 +40,10 @@ import com.jinjunhang.player.utils.ResourceHelper;
 /**
  * Base activity for activities that need to show a playback control fragment when media is playing.
  */
-public abstract class BaseActivity extends ActionBarCastActivity implements Album.MediaBrowserProvider {
+public abstract class BaseActivity extends SingleFragmentActivity {
 
     private static final String TAG = LogHelper.makeLogTag(BaseActivity.class);
 
-    private MediaBrowserCompat mMediaBrowser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,21 +51,6 @@ public abstract class BaseActivity extends ActionBarCastActivity implements Albu
 
         LogHelper.d(TAG, "Activity onCreate");
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            // Since our app icon has the same color as colorPrimary, our entry in the Recent Apps
-            // list gets weird. We need to change either the icon or the color
-            // of the TaskDescription.
-            ActivityManager.TaskDescription taskDesc = new ActivityManager.TaskDescription(
-                    getTitle().toString(),
-                    BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_white),
-                    ResourceHelper.getThemeColor(this, R.attr.colorPrimary,
-                            android.R.color.darker_gray));
-            setTaskDescription(taskDesc);
-        }
-
-        // Connect a media browser just to get the media session token. There are other ways
-        // this can be done, for example by sharing the session token directly.
-        mMediaBrowser = new MediaBrowserCompat(this, new ComponentName(this, MusicService.class), mConnectionCallback, null);
     }
 
     @Override
@@ -73,61 +59,12 @@ public abstract class BaseActivity extends ActionBarCastActivity implements Albu
         LogHelper.d(TAG, "Activity onStart");
 
 
-        mMediaBrowser.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         LogHelper.d(TAG, "Activity onStop");
-        if (getSupportMediaController() != null) {
-            getSupportMediaController().unregisterCallback(mMediaControllerCallback);
-        }
-        mMediaBrowser.disconnect();
     }
-
-    @Override
-    public MediaBrowserCompat getMediaBrowser() {
-        return mMediaBrowser;
-    }
-
-    protected void onMediaControllerConnected() {
-        // empty implementation, can be overridden by clients.
-    }
-
-    private void connectToSession(MediaSessionCompat.Token token) throws RemoteException {
-        MediaControllerCompat mediaController = new MediaControllerCompat(this, token);
-        setSupportMediaController(mediaController);
-        mediaController.registerCallback(mMediaControllerCallback);
-
-        onMediaControllerConnected();
-    }
-
-    // Callback that ensures that we are showing the controls
-    private final MediaControllerCompat.Callback mMediaControllerCallback =
-        new MediaControllerCompat.Callback() {
-            @Override
-            public void onPlaybackStateChanged(@NonNull PlaybackStateCompat state) {
-
-            }
-
-            @Override
-            public void onMetadataChanged(MediaMetadataCompat metadata) {
-
-            }
-        };
-
-    private final MediaBrowserCompat.ConnectionCallback mConnectionCallback =
-        new MediaBrowserCompat.ConnectionCallback() {
-            @Override
-            public void onConnected() {
-                LogHelper.d(TAG, "onConnected");
-                try {
-                    connectToSession(mMediaBrowser.getSessionToken());
-                } catch (RemoteException e) {
-                    LogHelper.e(TAG, e, "could not connect media controller");
-                }
-            }
-        };
 
 }
