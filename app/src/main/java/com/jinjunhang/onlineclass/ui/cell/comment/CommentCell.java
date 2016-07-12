30a93dd4027dc8459fc9c872b2ed6b047e6ca47b
jjhlzn <jinjunhang@hotmail.com>
2016-07-12 16:02:20 +0800

    bug fix

--- app/src/main/java/com/jinjunhang/onlineclass/ui/cell/comment/CommentCell.java
+++ app/src/main/java/com/jinjunhang/onlineclass/ui/cell/comment/CommentCell.java
@@ -1,8 +1,10 @@
 package com.jinjunhang.onlineclass.ui.cell.comment;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ import com.jinjunhang.onlineclass.R;
 import com.jinjunhang.onlineclass.model.Comment;
 import com.jinjunhang.onlineclass.service.ServiceConfiguration;
 import com.jinjunhang.onlineclass.ui.cell.BaseListViewCell;
+import com.jinjunhang.player.utils.LogHelper;
 import com.makeramen.roundedimageview.RoundedImageView;
 
 import com.ppi.emoji.EmojiTextView;
@@ -21,6 +24,7 @@ import com.ppi.emoji.emojiParser;
  * Created by lzn on 16/6/22.
  */
 public class CommentCell extends BaseListViewCell {
+    private static final String TAG = LogHelper.makeLogTag(CommentCell.class);
 
     private Comment mComment;
 
@@ -37,7 +41,16 @@ public class CommentCell extends BaseListViewCell {
         RoundedImageView image = ((RoundedImageView)v.findViewById(R.id.comment_user_image));
         image.setOval(true);
 
-        Glide.with(mActivity).load(ServiceConfiguration.GetUserProfileImage(mComment.getUserId())).into(image);
+        //Glide.with(mActivity).load(ServiceConfiguration.GetUserProfileImage(mComment.getUserId())).into(image);
+        Log.d(TAG, "url = " + ServiceConfiguration.GetUserProfileImage(mComment.getUserId()));
+        Glide
+                .with(mActivity)
+                .load(ServiceConfiguration.GetUserProfileImage(mComment.getUserId()))
+                .centerCrop()
+                .placeholder(R.drawable.smallusericon)
+                .crossFade()
+                .into(image);
+
 
         ((TextView)v.findViewById(R.id.comment_username)).setText(mComment.getNickName());
         ((TextView)v.findViewById(R.id.comment_date)).setText(mComment.getTime());
