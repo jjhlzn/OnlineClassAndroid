911f66632432b86ece22f40389cfdd2126c33171
jjhlzn <jinjunhang@hotmail.com>
2017-08-01 22:41:15 +0800

    html5页面的图片长按保存到相册

--- app/src/main/java/com/jinjunhang/onlineclass/ui/activity/WebBrowserActivity.java
+++ app/src/main/java/com/jinjunhang/onlineclass/ui/activity/WebBrowserActivity.java
@@ -2,13 +2,18 @@ package com.jinjunhang.onlineclass.ui.activity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.net.http.SslError;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
@@ -16,18 +21,34 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jinjunhang.framework.lib.LoadingAnimation;
 import com.jinjunhang.framework.lib.Utils;
+import com.jinjunhang.framework.service.BasicService;
 import com.jinjunhang.framework.wx.Util;
 import com.jinjunhang.onlineclass.R;
 import com.jinjunhang.onlineclass.db.LoginUserDao;
+import com.jinjunhang.onlineclass.model.Comment;
+import com.jinjunhang.onlineclass.model.LoginUser;
+import com.jinjunhang.onlineclass.service.GetFooterAdvsRequest;
+import com.jinjunhang.onlineclass.service.GetFooterAdvsResponse;
 import com.jinjunhang.onlineclass.ui.activity.user.LoginActivity;
+import com.jinjunhang.onlineclass.ui.cell.mainpage.FooterCell;
 import com.jinjunhang.onlineclass.ui.lib.ShareManager;
 import com.jinjunhang.framework.lib.LogHelper;
 import com.jinjunhang.onlineclass.ui.lib.ShareManager2;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by lzn on 16/6/20.
@@ -100,6 +121,8 @@ public class WebBrowserActivity extends AppCompatActivity {
         setWeixinShareIfNeed(findViewById(R.id.rootView));
         mWebView = (WebView) findViewById(R.id.webview);
 
+        registerForContextMenu(mWebView);
+
         mWebView.getSettings().setJavaScriptEnabled(true); // enable javascript
         mWebView.setWebViewClient(new MyWebViewClient());
 
@@ -145,7 +168,84 @@ public class WebBrowserActivity extends AppCompatActivity {
         mLoadingAnimation.hide();
     }
 
+
+    private byte[] downloadImage(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            InputStream is = (InputStream) url.getContent();
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+            while ((bytesRead = is.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+            return output.toByteArray();
+        } catch (MalformedURLException e) {
+            LogHelper.e(TAG, e);
+            return null;
+        } catch (IOException e) {
+            LogHelper.e(TAG, e);
+            return null;
+        }
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String imageUrl = params[0];
+            byte[] imageBytes = downloadImage(imageUrl);
+            if (imageBytes != null) {
+                Bitmap bm = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+                MediaStore.Images.Media.insertImage(getContentResolver(), bm, "" , "");
+
+
+                WebBrowserActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WebBrowserActivity.this, "保存成功", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        final WebView.HitTestResult result = mWebView.getHitTestResult();
+        MenuItem.OnMenuItemClickListener handler = new MenuItem.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                // handle on context menu click
+                LogHelper.d(TAG, "save image click");
+                String imageUrl = result.getExtra();
+                new DownloadImageTask().execute(imageUrl);
+                return true;
+            }
+        };
+
+        if (result.getType() == WebView.HitTestResult.IMAGE_TYPE ||
+                result.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+
+
+            menu.setHeaderTitle("");
+            menu.add(0, 666, 0, "保存图片").setOnMenuItemClickListener(handler);
+        }
+    }
+
+
+
     private class MyWebViewClient extends WebViewClient {
+
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             LogHelper.d(TAG, "url = " + url);
